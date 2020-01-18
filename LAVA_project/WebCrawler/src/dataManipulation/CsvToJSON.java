6f45f1fb8f7237671c512f12e86f70ce1e2fd0bc
Marie Bexte <marie.bexte@stud.uni-due.de>
2020-01-18 13:20:17 +0100

    #7 filtering according to exam type and location is now working

--- LAVA_project/WebCrawler/src/dataManipulation/CsvToJSON.java
+++ LAVA_project/WebCrawler/src/dataManipulation/CsvToJSON.java
@@ -19,7 +19,7 @@ public class CsvToJSON {
 	public static void main(String[] args) throws IOException {
 		
 //		BufferedReader br = new BufferedReader(new FileReader(new File("e3_courses_FINAL.csv")));
-		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("e3_courses_withLinks_final.json")));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("e3_courses_withLinks_cleanTypes_final.json")));
 		
 //		String line = br.readLine();
 //		String[] entries;
@@ -35,7 +35,7 @@ public class CsvToJSON {
 //			System.out.println("CAT: "+catalog);
 //		}
 		
-	    Reader in = new FileReader("e3_courses_reoccurrenceAndMyProf_reduced_withLinks.csv");
+	    Reader in = new FileReader("e3_courses_180120.csv");
 	    Iterable<CSVRecord> records = CSVFormat.DEFAULT
 //	      .withHeader(HEADERS)
 	      .withFirstRecordAsHeader()
